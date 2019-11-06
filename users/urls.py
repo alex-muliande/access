d2f594aca5e8e69b7b6cfe895112f3adeb3cf870
Leo <leoamby@gmail.com>
2019-11-06 11:41:36 +0300

    created index and base html files. Added the index view

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views
 
 
 
 urlpatterns = [
+    path('register/', views.register, name ='register'),
+    path('', views.index, name = 'index'),
 
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 
+
+
+def index(request):
+    return render(request, 'index.html')
+
 def register(response):
     if response.method == "POST":
         form = RegisterForm()
         if form.is_valid():
             form.save()
         return redirect("index")
+        
     else:
         form = RegisterForm()
     return render (response, "registration/signup.html", {"form":form})
