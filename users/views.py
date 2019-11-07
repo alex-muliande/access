1e3fbee1ecaf41be50f7e0312647d4caa9e3e573
Leo <leoamby@gmail.com>
2019-11-07 14:44:11 +0300

    more styling to the register forms and ammendments to the veiws and signup.html files

--- users/views.py
+++ users/views.py
@@ -12,10 +12,7 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-            # username = form.cleaned_data('username')
-            # email = form.cleaned_data('email')
-            # password = form.cleaned_data('password1')
-            # user = authenticate(username=username,email=email,password=password)
+           
         return redirect("login")
 
     else:
