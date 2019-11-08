efdba6e813947adf043347b788c21ec68a230b92
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-11-08 12:45:05 +0300

    Creates the initial form models.

--- access/models.py
+++ access/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+class InitialForm(models.Model):
+    cert_image = models.ImageField(upload_to = 'images')
+    name =models.CharField(max_length=30)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+
+    def get_absolute_url(self):
+        return reverse('index')
+
+
