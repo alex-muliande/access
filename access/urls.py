e9883d762dd203681f83829cc07430da6070a0d6
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-11-11 12:08:58 +0300

    create the welcome form of interst to send welcome email

--- access/urls.py
+++ access/urls.py
@@ -4,10 +4,9 @@ from django.urls import path, include
 from .views import InitialformCreateView
 from . import views
 
-
 urlpatterns=[
     path('', views.index, name ='index'),
-    path('initial/', InitialformCreateView.as_view(), name='initial')
+    path('initial/',views.initial, name='initial')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)  
--- moringa/settings.py
+++ moringa/settings.py
@@ -138,7 +138,6 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
