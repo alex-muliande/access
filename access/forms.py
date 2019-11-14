663968887f8de99a0aa5854545080b35ed327fbe
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-11-14 09:39:41 +0300

    create the initial formfor the applicants

--- access/forms.py
+++ access/forms.py
@@ -1,6 +1,12 @@
 from django import forms
-from .models import InitialForm
-class InitialformCreateView(forms.ModelForm):
+from .models import InitialForm,Try
+
+class InitialformCreateView(forms.ModelForm ):
     class Meta:
         model = InitialForm
         fields = ['cert_image','name','email']
+
+class FormsTry(forms.ModelForm):
+    class Meta:
+        model = Try
+        fields = ['name','email','age']
