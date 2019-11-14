3285170e9b7475222dcd81e0629c61b9c51db57d
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-11-14 11:23:30 +0300

    change view cer-image to kcse certiificate image

--- access/views.py
+++ access/views.py
@@ -5,6 +5,8 @@ from django.views.generic import CreateView
 from .models import InitialForm
 from django.http import HttpResponse
 from .forms import InitialformCreateView
+
+
 def index(request):
     return render(request, 'index.html')
 
@@ -12,10 +14,10 @@ def initial(request):
     if request.method == 'POST':
         form = InitialformCreateView(request.POST, request.FILES)
         if form.is_valid():
-            cert_image = form.cleaned_data['cert_image']
+            KCSE_certificate_image = form.cleaned_data['KCSE_certificate_image']
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
-            recipient = InitialForm(name = name, email = email,cert_image=cert_image)
+            recipient = InitialForm(KCSE_certificate_image=KCSE_certificate_image,name = name, email = email)
             recipient.save()
             welcome_to_moringa(name,email)
             HttpResponseRedirect('index')
