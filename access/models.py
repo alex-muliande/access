9b81fa6e6eb9e2d1efd7fd2fc6db8b870b1a6950
alex-muliande <amuliande@gmail.com>
2019-11-14 13:09:26 +0300

    Add  all mails to the models ...

--- access/models.py
+++ access/models.py
@@ -7,9 +7,17 @@ class InitialForm(models.Model):
     name =models.CharField(max_length=30)
     date_posted = models.DateTimeField(auto_now_add=True)
     email = models.EmailField(default = 'email@gmail.com')
+    is_sent= models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
+    @classmethod
+    def all_emails(cls):
+        list_emails=[]
+        mails= cls.objects.filter(is_sent=False).all()
+        for mail in mails:
+            list_emails.append(mail.email)
+        return list_emails
 
 
