3f1a57978e326a2279caa415d989b75b2d6e2018
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-11-14 11:22:25 +0300

    change model cert image to kcse cert image

--- access/models.py
+++ access/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 # Create your models here.
 class InitialForm(models.Model):
-    cert_image = models.ImageField(upload_to = 'media/images')
+    KCSE_certificate_image = models.ImageField(upload_to = 'media/images')
     name =models.CharField(max_length=30)
     date_posted = models.DateTimeField(auto_now_add=True)
     email = models.EmailField(default = 'email@gmail.com')
