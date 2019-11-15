c8f991fc536647588a665d73b9a9aca4edff4822
melisajuma <Melisaakinyi95@gmail.com>
2019-11-15 11:45:11 +0300

    Merge melisa to dev.

--- access/urls.py
+++ access/urls.py
@@ -1,26 +1,24 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-
-#router = routers.DefaultRouter()
-#router.register('', views.Profileview)
-
-urlpatterns = [
-path('homepage/',views.homepage,name='homepage'),
-path('assesment/',views.scorecard,name='assesment'),
-path('rejected/',views.failed,name='rejected'),
-path('accepted/',views.failed,name='accepted'),
-]
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from .views import InitialformCreateView
 from . import views
 
+#router = routers.DefaultRouter()
+#router.register('', views.Profileview)
+
+
+
 urlpatterns=[
     path('', views.index, name ='index'),
     path('initial/',views.initial, name='initial'),
-    path('bulk/',views.congragulate)
+    path('bulk/',views.congragulate),
+    path('assesment/',views.scorecard,name='assesment'),
+    path('rejected/',views.failed,name='rejected'),
+    path('accepted/',views.failed,name='accepted'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)  
