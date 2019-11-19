01a792c6d3e94a9b8141efd5a6ac0309204c3715
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-11-19 19:11:26 +0300

    create a view function to send emails to rejected users

--- access/forms.py
+++ access/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import InitialForm
+from .models import InitialForm,KnowMoringa
 
 class InitialformCreateView(forms.ModelForm ):
     class Meta:
         model = InitialForm
         fields = ['KCSE_certificate_image','your_name','email']
 
+class MoreInformation(forms.ModelForm):
+    class Meta:
+        model = KnowMoringa
+        fields = ['name','email']
