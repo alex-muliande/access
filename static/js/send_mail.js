82f73dd1d0f91e2eb5616a5113445cd433078e97
alex-muliande <amuliande@gmail.com>
2019-11-21 14:12:38 +0300

    add functions to send bulk mails to the send bulk.js

--- static/js/send_mail.js
+++ static/js/send_mail.js
@@ -80,7 +80,7 @@ $(document).ready(function(){
     })
     
 })
-
+// send congratulatulation4
 $(document).ready(function(){
     console.log('I am ready');
     $(document).ajaxStart(function(){
@@ -92,6 +92,69 @@ $(document).ready(function(){
     
     $.ajax({
         url:'/congrats4/',
+        method:'GET',
+        beforeSend:function(){
+        $('.loader').css({'display':'block'})
+        console.log('I have gone');
+
+        
+        }
+    })
+    .done(function(data){
+        $('.loader').css ({'display':'none'})
+        console.log('I am done');
+        console.log(data.sent);
+        
+        
+    })
+})
+})
+
+// send congratulation6
+
+$(document).ready(function(){
+    console.log('I am ready');
+    $(document).ajaxStart(function(){
+        $('.loader').css({'display':'block'})
+    })
+    $('.congrats6').click(function(){
+
+   
+    
+    $.ajax({
+        url:'/congrats6/',
+        method:'GET',
+        beforeSend:function(){
+        $('.loader').css({'display':'block'})
+        console.log('I have gone');
+
+        
+        }
+    })
+    .done(function(data){
+        $('.loader').css ({'display':'none'})
+        console.log('I am done');
+        console.log(data.sent);
+        
+        
+    })
+})
+})
+
+
+// Send email to all rejected people
+
+$(document).ready(function(){
+    console.log('I am ready');
+    $(document).ajaxStart(function(){
+        $('.loader').css({'display':'block'})
+    })
+    $('.rejected').click(function(){
+
+   
+    
+    $.ajax({
+        url:'/rejected/',
         method:'GET',
         beforeSend:function(){
         $('.loader').css({'display':'block'})
