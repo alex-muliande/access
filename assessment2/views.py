002697c097d4eacaa561964308baa3230470db33
melisajuma <Melisaakinyi95@gmail.com>
2019-11-26 17:10:47 +0300

    changes

--- assessment2/views.py
+++ assessment2/views.py
@@ -36,12 +36,8 @@ def accepted(request):
     for f in passed:
         for email in  scoreModel.objects.values_list('email', flat=True).distinct():
             scoreModel.objects.filter(pk__in= scoreModel.objects.filter(email=email).values_list('id', flat=True)[1:]).delete()
-<<<<<<< HEAD
     return render(request,'accepted.html',{'passed':passed,'failed':failed})
 
-=======
-    return render(request,'accepted.html',{'passed':passed, 'failed':failed})
->>>>>>> b15ca23244a83e6a4c5ea4c3c47d8358f3806592
 
 def send_bulk4(email,name):
     # connection = EmailMultiAlternatives.get_connection()
@@ -86,10 +82,7 @@ def congragulate4(request):
                 pass
         return JsonResponse({'sent':users_emails4})
     return JsonResponse({'sent':'upto date'})
-<<<<<<< HEAD
 
 
 
     
-=======
->>>>>>> b15ca23244a83e6a4c5ea4c3c47d8358f3806592
